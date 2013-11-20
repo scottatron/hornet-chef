5a54f91780defccb8aecc23a3073b74685d52faa
Scott Arthur <scott@scottatron.com>
2013-11-21 00:03:06 +1100

    APT

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name             'hornet'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Scott Arthur'
+maintainer_email 'scott@scottatron.com'
 license          'All rights reserved'
 description      'Installs/Configures hornet'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'apt', '~> 2.3'
