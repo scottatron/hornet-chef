4d6b8aaa2f944d0d00e5044d3dd210eee9090a19
Scott Arthur <scott@scottatron.com>
2014-05-05 18:09:02 +1000

    Moved Ruby version into an attribute

---
+++ attributes/ruby.rb
@@ -0,0 +1,1 @@
+default.hornet.ruby.version = '1.9.3-p545'
--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -1,10 +1,10 @@
 include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
 
-rbenv_ruby "1.9.3-p484" do
+rbenv_ruby node.hornet.ruby.version do
   global true
 end
 
 rbenv_gem "bundler" do
-  ruby_version "1.9.3-p484"
+  ruby_version node.hornet.ruby.version
 end
