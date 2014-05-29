a80ba205a41cc7f14227f2c8d24bde94c702fe25
Scott Arthur <scott@scottatron.com>
2014-05-29 13:08:10 +1200

    Default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,16 +1,3 @@
-#
-# Cookbook Name:: hornet
-# Recipe:: default
-#
-# Copyright (C) 2013 YOUR_NAME
-# 
-# All rights reserved - Do Not Redistribute
-#
-
-include_recipe "apt"
-include_recipe "git"
-
-package "libsqlite3-dev"
-package "imagemagick"
-
-include_recipe "sudo"
+include_recipe 'hornet::ruby'
+include_recipe 'mysql::server'
+package 'imagemagick'
